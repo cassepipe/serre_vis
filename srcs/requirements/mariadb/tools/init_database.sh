7616ab52e7e8db2d252c6bf4a3f4fd1c9dbfcb42
cassepipe <cassepipe@ymail.com>
2022-10-09 01:09:36 +0200

    Remove unnecessary checks

--- srcs/requirements/mariadb/tools/init_database.sh
+++ srcs/requirements/mariadb/tools/init_database.sh
@@ -5,10 +5,12 @@ green='\e[32m'
 cyan='\e[36m'
 nocolor='\e[0m'
 
-create_wordpress_database_user()
+create_wordpress_database()
 {
 	echo -e $cyan"Creating wordpress user..." $nocolor
-	mysql -u root --password=$MYSQL_ROOT_PASSWORD <<-EOF
+	mysql -u root --password=$MYSQL_ROOT_PASSWORD \
+			<<-EOF
+			CREATE DATABASE IF NOT EXISTS $MYSQL_DATABASE;
 			CREATE USER IF NOT EXISTS $MYSQL_USER;
 			GRANT ALL PRIVILEGES ON $MYSQL_DATABASE.* TO "$MYSQL_USER"@"%" IDENTIFIED BY "$MYSQL_PASSWORD";
 			FLUSH PRIVILEGES;
@@ -32,13 +34,13 @@ secure_database()
 main()
 {
 	mysqld &
-	sleep 1
-	if mysql -e "CREATE DATABASE $MYSQL_DATABASE"; then
-		create_wordpress_database_user && echo -e $cyan"Database and database user created" $nocolor || return
+	sleep 2
+	#if mysql -e "CREATE DATABASE $MYSQL_DATABASE"; then
+		create_wordpress_database && echo -e $cyan"Database and database user created" $nocolor || return
 		secure_database && echo -e $cyan "Database secured" $nocolor || return
-	else
-		echo -e $green"Database already exists" $nocolor
-	fi
+	#else
+	#    echo -e $green"Database already exists" $nocolor
+	#fi
 	echo -e $cyan"Here are the current databases :" $nocolor
 	mysqlshow -u root --password=$MYSQL_ROOT_PASSWORD
 	echo -e $cyan"Here are the current users@host :" $nocolor
